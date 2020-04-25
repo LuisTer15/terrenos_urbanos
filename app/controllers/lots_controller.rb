37259c37e99b37e7df4ba826201af4cf8ce7e2a9
Luis Ternera <lternerah@gmail.com>
2020-04-25 11:13:45 -0500

    Adds delete functionality to lots items

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -33,6 +33,12 @@ class LotsController < ApplicationController
 		end
 	end
 
+	def destroy
+		lot = Lot.find(params[:id])
+		lot.destroy
+		redirect_to lots_path, notice: "El lote fue eleminado con éxito"
+	end
+
 	private
 		def lot_params
 			params.require(:lot).permit(:code, :location, :address, :area_land, :area_build, :front_dim, :floors_build, :use_build)
