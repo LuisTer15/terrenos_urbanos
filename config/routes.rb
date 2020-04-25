89e02488351a2f990db22ddc91384ff6bed26c1b
Luis Ternera <lternerah@gmail.com>
2020-04-25 11:31:20 -0500

    Use resources for Lots routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   get '/app_pages/app'
 
-  get '/lots', to: 'lots#index'
-  get '/lots/new', to: 'lots#new', as: 'new_lot'
-  post '/lots', to: 'lots#create'
-  get '/lots/:id', to: 'lots#show', as: 'lot'
-  get '/lots/:id/edit', to: 'lots#edit', as: 'edit_lot'
-  patch '/lots/:id', to: 'lots#update'
-  delete '/lots/:id', to: 'lots#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lots
+  #get '/lots', to: 'lots#index'
+  #get '/lots/new', to: 'lots#new', as: 'new_lot'
+  #post '/lots', to: 'lots#create'
+  #get '/lots/:id', to: 'lots#show', as: 'lot'
+  #get '/lots/:id/edit', to: 'lots#edit', as: 'edit_lot'
+  #patch '/lots/:id', to: 'lots#update'
+  #delete '/lots/:id', to: 'lots#destroy'
 end
