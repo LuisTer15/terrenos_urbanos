723b0a2d6426e20fddc5912e29480a1d2491fcea
Luis Ternera <lternerah@gmail.com>
2020-04-25 10:20:39 -0500

    Adds patch funtionality to update a lot

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -24,6 +24,16 @@ class LotsController < ApplicationController
 		@lot = Lot.find(params[:id])
 	end
 
+	def update
+		@lot = Lot.find(params[:id])
+		if @lot.update(lot_params)
+			redirect_to lots_path, notice: "El lote ha sido actualizaod con éxito"
+		else
+			render :edit
+		end
+	end
+
+
 	private
 		def lot_params
 			params.require(:lot).permit(:code, :location, :address, :area_land, :area_build, :front_dim, :floors_build, :use_build)
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post '/lots', to: 'lots#create'
   get '/lots/:id', to: 'lots#show', as: 'lot'
   get '/lots/:id/edit', to: 'lots#edit'
+  patch '/lots/:id', to: 'lots#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
