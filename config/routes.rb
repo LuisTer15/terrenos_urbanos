1149687f637b851e98ae2c0a7af2aac5475def12
Luis Ternera <lternerah@gmail.com>
2020-04-25 13:42:51 -0500

    Adds logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/app_pages/app'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
   resources :lots
