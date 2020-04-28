e972e5e0d4c8b6db8d1732ee5655ee422c2bbf02
Luis Ternera <lternerah@gmail.com>
2020-04-28 13:28:16 -0500

    Adds validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	has_secure_password validations: false
+
+	validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nill: true
+	validates :name, presence: true
 end
