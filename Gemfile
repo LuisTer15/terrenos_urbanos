6a6c30f4af92adbd09da9c28471a0729bc3799c3
LuisTer15 <lternerah@gmail.com>
2021-04-03 13:24:06 -0500

    Seeds blog database

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'popper_js', '~> 1.14.5'
+gem 'coffee-script-source', '1.8.0'
 
 group :development, :test do
   # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Blog.create(title: 'Blog de prueba', description: 'Este es el primer blog de rpueba, Leer mas...', content: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?")
