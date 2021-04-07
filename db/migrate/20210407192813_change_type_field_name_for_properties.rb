14df4ecaf2eaca8f775e5110880f195c58208d7f
LuisTer15 <lternerah@gmail.com>
2021-04-07 18:13:15 -0500

    Deletes mistaken migrations

--- db/migrate/20210407192813_change_type_field_name_for_properties.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTypeFieldNameForProperties < ActiveRecord::Migration[5.1]
-  def change
-  	change_column :properties, :type, :property_type
-  end
-end
--- db/migrate/20210407194016_fix_field_type_for_properties.rb
+++
@@ -1,5 +0,0 @@
-class FixFieldTypeForProperties < ActiveRecord::Migration[5.1]
-  def change
-  	change_column :properties, :type, :string
-  end
-end
