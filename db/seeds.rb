b4a5bf36a61d51477b08c651a4362c843237370e
LuisTer15 <lternerah@gmail.com>
2021-04-08 13:01:34 -0500

    Adds links to views for MVCs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Blog.create(title: 'Blog de prueba', description: 'Este es el primer blog de rpueba, Leer mas...', content: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?")
+Blog.create(title: 'Blog de prueba', 
+			description: 'Este es el primer blog de rpueba, Leer mas...', 
+			content: "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?")
